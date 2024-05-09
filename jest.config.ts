89fd6b23ee9221b543e3134c54d3a0a709eb7fe6
Eugenumber1 <y.budnyk789@gmail.com>
2024-05-09 20:49:58 +0200

    added background.test

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,10 @@ import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
   preset: "ts-jest",
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: [
+    "jest-fetch-mock/setupJest"
+  ],
   roots: ["<rootDir>/src"],
   testMatch: ["**/*.test.ts", "**/*.spec.ts"],
 };
--- src/background/background.ts
+++ src/background/background.ts
@@ -1,16 +1,29 @@
 import OpenAI from 'openai';
 
+require('dotenv').config();
 const openai = new OpenAI({
   apiKey: process.env['OPENAI_API_KEY'],
+  dangerouslyAllowBrowser: true
 });
 
-export default async function fetchExplanation(content: string) {
+
+async function fetchExplanation(content: string) {
   const chatCompletion = await openai.chat.completions.create({
     messages: [{ role: 'user', content: content },{role: 'system', content:"Explain the input text in averagely simple words. Maximum 50 - 70 words."}],
     model: 'gpt-3.5-turbo',
   });
-  return chatCompletion.choices[0].message.content
+  console.log(chatCompletion);
+  console.log(chatCompletion["choices"][0]["message"]["content"]);
+  // console.log(chatCompletion.choices[0].message);
+  return chatCompletion["choices"][0]["message"]["content"]
 }
 
 
-console.log(fetchExplanation("Iphone has a very specific design pattern of a smooth stone."));
+export async function run(content: string) {
+  try {
+    const explanation = await fetchExplanation(content);
+    console.log(explanation);
+  } catch (error) {
+    console.error("Error fetching explanation:", error);
+  }
+}
